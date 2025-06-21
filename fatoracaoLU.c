b75974d01ffb810140738320a013c31403dc10b5
Raphael Secchin <158609068+Secchinho@users.noreply.github.com>
2025-06-21 01:43:14 -0300

    Update fatoracaoLU.c

--- fatoracaoLU.c
+++ fatoracaoLU.c
@@ -59,28 +59,3 @@ void resolverLU(double **matrizL, double **matrizU, double *b, double *x, double
         x[i] /= matrizU[i][i];
     }
 }
-
-void substituicaoLy(double **matrizL, double *b, double *y, int ordemMat)
-{
-    for(int i = 0; i < ordemMat; i ++)
-    {
-        y[i] = b[i];
-        for(int j = 0; j < i; j++)
-        {
-            y[i] -= matrizL[i][j] * y[j];
-        }
-    }
-}
-
-void substituicaoUx(double **matrizU, double *y, double *x, int ordemMat)
-{
-    for(int i = ordemMat - 1; i >= 0; i--)
-    {
-        x[i] = y[i];
-        for(int j = i + 1; j < ordemMat; j++)
-        {
-            x[i] -= matrizU[i][j] * x[j];
-        }
-        x[i] /= matrizU[i][i];
-    }
-}
