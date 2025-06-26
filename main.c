af974c363df68da897185af72ba9b404ef5fcae0
Andre-D-S <andrestefanato@gmail.com>
2025-06-26 07:49:54 -0300

    main.c alterado e correto

--- main.c
+++ main.c
@@ -7,17 +7,16 @@
 #include "gaussSeidel.h"
 #include "gaussJacobi.h"
 
-
 float** alocacaoMat(int ordemMat)
 {
-    float **matriz = malloc(ordemMat * sizeof(float* ));
+    float **matriz = malloc(ordemMat * sizeof(float*));
     for(int i = 0; i < ordemMat; i++)
     {
         matriz[i] = calloc(ordemMat, sizeof(float));
     }
-
     return matriz;
 }
+
 
 void liberarMat(float **matriz, int ordemMat)
 {
@@ -28,20 +27,29 @@ void liberarMat(float **matriz, int ordemMat)
     free(matriz);
 }
 
+
+void copiarMatriz(float **origem, float **destino, int ordem)
+{
+    for (int i = 0; i < ordem; i++) {
+        for (int j = 0; j < ordem; j++) {
+            destino[i][j] = origem[i][j];
+        }
+    }
+}
+
 int main(int argc, char *argv[])
 {
     if (argc != 2)
     {
+        printf("Uso: %s <arquivo_entrada>\n", argv[0]);
         exit(1);
     }
     
     clock_t inicio, fim;
 
     int sistemas = 0;
-    int contSistemas = 0;
     int ordemMat = 0;
     float precisao = 0;
-    float tempo = 0;
 
     FILE *arq = fopen(argv[1], "r");
 
@@ -51,17 +59,19 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
-    //lendo a quantidade de sistemas
+    //Lendo a quantidade de sistemas
     fscanf(arq, "%d", &sistemas);
-
-    //lendo o tamanho da matriz e a precisão
+    
+    //Lendo o tamanho da matriz e a precisão
     fscanf(arq, "%d %f", &ordemMat, &precisao);
 
+    //Alocação das matrizes
     float **matriz = alocacaoMat(ordemMat);
     float **matrizL = alocacaoMat(ordemMat);
     float **matrizU = alocacaoMat(ordemMat);
+    float **matrizCopia = alocacaoMat(ordemMat);
 
-     //preenchendo a matriz
+    //Preenchendo a matriz
     for(int i = 0; i < ordemMat; i++)
     {
         for(int j = 0; j < ordemMat; j++)
@@ -70,12 +80,23 @@ int main(int argc, char *argv[])
         }
     }
 
-    
+    //Leitura dos vetores B para cada sistema
+    float **bSistemas = malloc(sistemas * sizeof(float*));
+    for (int s = 0; s < sistemas; s++) 
+    {
+        bSistemas[s] = malloc(ordemMat * sizeof(float));
+        for (int i = 0; i < ordemMat; i++) {
+            fscanf(arq, "%f", &bSistemas[s][i]);
+        }
+    }
+
     float *b = calloc(ordemMat, sizeof(float));
     float *x = calloc(ordemMat, sizeof(float));
     float *y = calloc(ordemMat, sizeof(float));
+
+    //=================== Eliminação de Gauss ===================
+    printf("\nResultados e tempo de execução da Eliminação de Gauss:\n");
     
-    printf("\nResultados e tempo de execução da Eliminação de Gauss:");
     inicio = clock();
     
     for (int s = 0; s < sistemas; s++) 
@@ -83,48 +104,56 @@ int main(int argc, char *argv[])
         printf("\nSistema %d:\n", s + 1);
         
         for (int i = 0; i < ordemMat; i++)
-        {
-            fscanf(arq, "%f", &b[i]);
-        }
-        
-        gaussEliminacaoPivotamento(ordemMat, matriz, b, x);
-        
-        printf("Vetor solução X:\n");
-        for (int i = 0; i < ordemMat; i++)
-        {
-            printf("x[%d] = %f\n", i, x[i]);
-        }
-    }
-    fim = clock();
+            b[i] = bSistemas[s][i];
 
+        copiarMatriz(matriz, matrizCopia, ordemMat);
+        
+        gaussEliminacaoPivotamento(ordemMat, matrizCopia, b, x);
+
+        printf("Vetor solução X:\n");
+        
+        for (int i = 0; i < ordemMat; i++)
+            printf("x[%d] = %f\n", i, x[i]);
+    }
+    
+    fim = clock();
+    
     printf("Tempo de execução Eliminação de Gauss: %f\n", (float)(fim - inicio) / CLOCKS_PER_SEC);
 
-    printf("\nResultados e tempo de execução da Fatoração LU:");
+    //=================== Fatoração LU ===================
+    printf("\nResultados e tempo de execução da Fatoração LU:\n");
+    
     inicio = clock();
-    fatoracaoLU(matriz, matrizL, matrizU, ordemMat);
+    
+    copiarMatriz(matriz, matrizCopia, ordemMat);
+    
+    fatoracaoLU(matrizCopia, matrizL, matrizU, ordemMat);
     
     for (int s = 0; s < sistemas; s++) 
     {
         printf("\nSistema %d:\n", s + 1);
         
         for (int i = 0; i < ordemMat; i++)
-        {
-            fscanf(arq, "%f", &b[i]);
-        }
+            b[i] = bSistemas[s][i];
+
+        for (int i = 0; i < ordemMat; i++) 
+            x[i] = y[i] = 0;
         
         resolverLU(matrizL, matrizU, b, x, y, ordemMat);
-        
-        printf("Vetor solução X:\n");
-        for (int i = 0; i < ordemMat; i++)
-        {
-            printf("x[%d] = %f\n", i, x[i]);
-        }
-    }
-    fim = clock();
 
+        printf("Vetor solução X:\n");
+        
+        for (int i = 0; i < ordemMat; i++)
+            printf("x[%d] = %f\n", i, x[i]);
+    }
+    
+    fim = clock();
+    
     printf("Tempo de execução FatoraçãoLU: %f\n", (float)(fim - inicio) / CLOCKS_PER_SEC);
 
-    printf("\nResultados e tempo de execução da Gauss Seidel:");
+    //=================== Gauss-Seidel ===================
+    printf("\nResultados e tempo de execução da Gauss Seidel:\n");
+    
     inicio = clock();
     
     for (int s = 0; s < sistemas; s++) 
@@ -132,23 +161,28 @@ int main(int argc, char *argv[])
         printf("\nSistema %d:\n", s + 1);
         
         for (int i = 0; i < ordemMat; i++)
-        {
-            fscanf(arq, "%f", &b[i]);
-        }
-        
-        gaussSeidel(ordemMat, matriz, b, x, precisao);
-        
-        printf("Vetor solução X:\n");
-        for (int i = 0; i < ordemMat; i++)
-        {
-            printf("x[%d] = %f\n", i, x[i]);
-        }
-    }
-    fim = clock();
+            b[i] = bSistemas[s][i];
 
+        copiarMatriz(matriz, matrizCopia, ordemMat);
+        
+        for (int i = 0; i < ordemMat; i++) 
+            x[i] = 0;
+        
+        gaussSeidel(ordemMat, matrizCopia, b, x, precisao);
+
+        printf("Vetor solução X:\n");
+        
+        for (int i = 0; i < ordemMat; i++)
+            printf("x[%d] = %f\n", i, x[i]);
+    }
+    
+    fim = clock();
+    
     printf("Tempo de execução GaussSeidel: %f\n", (float)(fim - inicio) / CLOCKS_PER_SEC);
 
-    printf("\nResultados e tempo de execução da Gauss Jacobi:");
+    //=================== Gauss-Jacobi ===================
+    printf("\nResultados e tempo de execução da Gauss Jacobi:\n");
+    
     inicio = clock();
     
     for (int s = 0; s < sistemas; s++) 
@@ -156,28 +190,38 @@ int main(int argc, char *argv[])
         printf("\nSistema %d:\n", s + 1);
         
         for (int i = 0; i < ordemMat; i++)
-        {
-            fscanf(arq, "%f", &b[i]);
-        }
-        
-        gaussJacobi(ordemMat, matriz, b, x, precisao);
-        
-        printf("Vetor solução X:\n");
-        for (int i = 0; i < ordemMat; i++)
-        {
-            printf("x[%d] = %f\n", i, x[i]);
-        }
-    }
-    fim = clock();
+            b[i] = bSistemas[s][i];
 
+        copiarMatriz(matriz, matrizCopia, ordemMat);
+        
+        for (int i = 0; i < ordemMat; i++) 
+            x[i] = 0;
+        
+        gaussJacobi(ordemMat, matrizCopia, b, x, precisao);
+
+        printf("Vetor solução X:\n");
+        
+        for (int i = 0; i < ordemMat; i++)
+            printf("x[%d] = %f\n", i, x[i]);
+    }
+    
+    fim = clock();
+    
     printf("Tempo de execução GaussJacobi: %f\n", (float)(fim - inicio) / CLOCKS_PER_SEC);
 
+    //Liberação de memória
     liberarMat(matriz, ordemMat);
     liberarMat(matrizL, ordemMat);
     liberarMat(matrizU, ordemMat);
+    liberarMat(matrizCopia, ordemMat);
+    
     free(b);
     free(x);
     free(y);
+    
+    for (int s = 0; s < sistemas; s++) free(bSistemas[s]);
+    free(bSistemas);
     fclose(arq);
+
     return 0;
 }
